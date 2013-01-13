1f238ce797bda3c124df6704c1f16bcb59644605
bouzuya <bouzuya@gmail.com>
2013-01-13 11:21:06 +0900

    renamed core.clj -> tokenizer.clj

--- src/kuromoji_example/tokenizer.clj
+++ src/kuromoji_example/tokenizer.clj
@@ -1,8 +1,9 @@
-(ns kuromoji-example.core
+(ns kuromoji-example.tokenizer
   (:import [org.atilika.kuromoji Token Tokenizer]))
 
-(defn -main
-  [& args]
+; http://www.atilika.com/products/kuromoji.html#code-example
+(defn example
+  []
   (let [^Tokenizer tokenizer (.build (Tokenizer/builder))]
     (doseq [^Token token (.tokenize tokenizer "寿司が食べたい。")]
       (println (str (.getSurfaceForm token) "\t" (.getAllFeatures token))))))
